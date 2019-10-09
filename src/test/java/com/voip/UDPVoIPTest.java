4646813a539126ee2569dc69987488292ffd6968
the-DJDJ <jenkins.daniel.02@gmail.com>
2019-10-09 23:51:04 +0200

    [fix] Fixed an issue where the test caused a build to fail.

--- src/test/java/com/voip/UDPVoIPTest.java
+++ src/test/java/com/voip/UDPVoIPTest.java
@@ -25,7 +25,7 @@ public class UDPVoIPTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( UDPVoIPTest.class );
     }
 
     /**
